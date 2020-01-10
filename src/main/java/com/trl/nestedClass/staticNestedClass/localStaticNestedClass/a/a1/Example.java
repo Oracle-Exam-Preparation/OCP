a62e3eeafcf1300e7e8bd24fc0289591fc898c9b
TRL <tsyupryk.roman.lyubomyrovych@gmail.com>
2020-01-10 12:34:45 +0100

    Local Inner Class.

--- src/main/java/com/trl/nestedClass/staticNestedClass/localStaticNestedClass/a/a1/Example.java
+++ src/main/java/com/trl/nestedClass/staticNestedClass/localStaticNestedClass/a/a1/Example.java
@@ -34,4 +34,14 @@ class Outer {
     static {
 //        static class LocalStaticNestedClass { }                                               // Compilation ERROR !!!
     }
+
+    static {
+        /*
+         * This is a difficult situation that I could not solve. This area is static. Because of this, this method
+         * belongs to the "class type" and not "object type". Because of this, we can assume that, "LocalInnerClass"
+         * which is defined in this method is an "LocalStaticNestedClass".
+         * */
+        class LocalStaticNestedClass {
+        }
+    }
 }
